e1fd09d5dd03cabb5df20d140cedb4cd8cc5d429
Ocheezyy <miniod2018@gmail.com>
2021-03-30 10:31:35 -0400

    Updated folder structure of Layout.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -18,7 +18,7 @@ const burger = (props) => {
     if (transformedIngredients.length === 0) {
         transformedIngredients = <p>Please add some ingredients</p>
     }
-    console.log(transformedIngredients);
+    // console.log(transformedIngredients);
 
     return (
         <div className="Burger">
