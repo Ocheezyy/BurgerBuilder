916a3e171a392c8f8597c53c1494e2ab4016f7c3
Ocheezyy <miniod2018@gmail.com>
2021-04-06 12:47:07 -0400

    Updated the contactdata container to dynamically render input elements.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,32 +1,91 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Button/Button";
 import "./ContactData.css";
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                            {value: 'fastest', displayValue: 'Fastest'},
+                            {value: 'cheapest', displayValue: 'Cheapest'}
+                        ]
+                },
+                value: ''
+            },
         }
     }
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
+        let form = (
+            <form>
+                {formElementsArray.map(formElement => (
+                    <Input
+                        key={formElement.id}
+                        inputtype={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                    />
+                ))}
+                <Button btnType="Success">ORDER</Button>
+            </form>
+        )
+
         return (
             <div className="ContactData">
                 <h4>Please enter your contact information</h4>
-                <form>
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" placeholder="John Doe"/>
-                    <label htmlFor="email">E-Mail: </label>
-                    <input type="text" name="email" placeholder="johndoe1@email.com"/>
-                    <label htmlFor="street">Street: </label>
-                    <input type="text" name="street" placeholder="123 Home St"/>
-                    <label htmlFor="postal-code">Postal Code: </label>
-                    <input type="text" name="postal-code"/>
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
